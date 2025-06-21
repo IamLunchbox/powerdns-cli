e38e4ec7ce7cc9f88f21a3db0bb190a1d06f3c1c
IamLunchbox <r.grieger@hotmail.com>
2025-06-21 22:56:48 +0200

    Refactor MockUtils into own testutils_module

---
+++ tests/powerdns_cli_test_utils/testutils.py
@@ -0,0 +1,39 @@
+import requests
+from unittest.mock import MagicMock as unittest_MagicMock
+
+
+class MockUtils:
+    def __init__(self, mocker):
+        self.mocker = mocker
+
+    def mock_http_get(self, status_code: int, json_output: dict = None, text_output: str = '') -> unittest_MagicMock:
+        mock_autoprimary_get = self.mocker.MagicMock(spec=requests.Response)
+        mock_autoprimary_get.json.return_value = json_output
+        mock_autoprimary_get.text.return_value = text_output
+        mock_autoprimary_get.status_code = status_code
+        mock_autoprimary_get.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_get', return_value=mock_autoprimary_get)
+
+    def mock_http_post(self, status_code: int, json_output: dict = {}, text_output: str = '') -> unittest_MagicMock:
+        mock_autoprimary_post = self.mocker.MagicMock(spec=requests.Response)
+        mock_autoprimary_post.json.return_value = json_output
+        mock_autoprimary_post.text.return_value = text_output
+        mock_autoprimary_post.status_code = status_code
+        mock_autoprimary_post.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_post', return_value=mock_autoprimary_post)
+
+    def mock_http_delete(self, status_code: int, json_output: dict = {}, text_output: str = '') -> unittest_MagicMock:
+        mock_autoprimary_delete = self.mocker.MagicMock(spec=requests.Response)
+        mock_autoprimary_delete.json.return_value = json_output
+        mock_autoprimary_delete.text.return_value = text_output
+        mock_autoprimary_delete.status_code = status_code
+        mock_autoprimary_delete.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_delete', return_value=mock_autoprimary_delete)
+
+    def mock_http_put(self, status_code: int, json_output: dict = {}, text_output: str = '') -> unittest_MagicMock:
+        mock_autoprimary_put = self.mocker.MagicMock(spec=requests.Response)
+        mock_autoprimary_put.json.return_value = json_output
+        mock_autoprimary_put.text.return_value = text_output
+        mock_autoprimary_put.status_code = status_code
+        mock_autoprimary_put.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_post', return_value=mock_autoprimary_put)
--- tests/test_autoprimary.py
+++ tests/test_autoprimary.py
@@ -1,42 +1,14 @@
+import json
+
 import pytest
-import requests
 from click.testing import CliRunner
 from powerdns_cli.powerdns_cli import autoprimary_add, autoprimary_list, autoprimary_delete
-import json
-from unittest.mock import MagicMock as unittest_MagicMock
+from powerdns_cli_test_utils import testutils
 
-
-class MockUtils:
-    def __init__(self, mocker):
-        self.mocker = mocker
-
-    def mock_http_get(self, status_code: int, json_output: dict = {}, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_get = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_get.json.return_value = json_output
-        mock_autoprimary_get.text.return_value = text_output
-        mock_autoprimary_get.status_code = status_code
-        mock_autoprimary_get.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_get', return_value=mock_autoprimary_get)
-
-    def mock_http_post(self, status_code: int, json_output: dict = {}, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_post = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_post.json.return_value = json_output
-        mock_autoprimary_post.text.return_value = text_output
-        mock_autoprimary_post.status_code = status_code
-        mock_autoprimary_post.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_post', return_value=mock_autoprimary_post)
-
-    def mock_http_delete(self, status_code: int, json_output: dict = {}, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_delete = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_delete.json.return_value = json_output
-        mock_autoprimary_delete.text.return_value = text_output
-        mock_autoprimary_delete.status_code = status_code
-        mock_autoprimary_delete.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_delete', return_value=mock_autoprimary_delete)
 
 @pytest.fixture
 def mock_utils(mocker):
-    return MockUtils(mocker)
+    return testutils.MockUtils(mocker)
 
 
 def test_autoprimary_add_success(mock_utils):
