2c7427f8ed999bf8cfa972a56c453dde52207f61
IamLunchbox <r.grieger@hotmail.com>
2025-06-22 23:14:37 +0200

    Use correct function names in MockUtils

--- tests/powerdns_cli_test_utils/testutils.py
+++ tests/powerdns_cli_test_utils/testutils.py
@@ -7,33 +7,33 @@ class MockUtils:
         self.mocker = mocker
 
     def mock_http_get(self, status_code: int, json_output: dict | list = None, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_get = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_get.json.return_value = json_output
-        mock_autoprimary_get.text.return_value = text_output
-        mock_autoprimary_get.status_code = status_code
-        mock_autoprimary_get.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_get', return_value=mock_autoprimary_get)
+        mock_http_get = self.mocker.MagicMock(spec=requests.Response)
+        mock_http_get.json.return_value = json_output
+        mock_http_get.text.return_value = text_output
+        mock_http_get.status_code = status_code
+        mock_http_get.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_get', return_value=mock_http_get)
 
     def mock_http_post(self, status_code: int, json_output: dict | list = None, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_post = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_post.json.return_value = json_output
-        mock_autoprimary_post.text.return_value = text_output
-        mock_autoprimary_post.status_code = status_code
-        mock_autoprimary_post.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_post', return_value=mock_autoprimary_post)
+        mock_http_post = self.mocker.MagicMock(spec=requests.Response)
+        mock_http_post.json.return_value = json_output
+        mock_http_post.text.return_value = text_output
+        mock_http_post.status_code = status_code
+        mock_http_post.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_post', return_value=mock_http_post)
 
     def mock_http_delete(self, status_code: int, json_output: dict | list = None, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_delete = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_delete.json.return_value = json_output
-        mock_autoprimary_delete.text.return_value = text_output
-        mock_autoprimary_delete.status_code = status_code
-        mock_autoprimary_delete.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_delete', return_value=mock_autoprimary_delete)
+        mock_http_delete = self.mocker.MagicMock(spec=requests.Response)
+        mock_http_delete.json.return_value = json_output
+        mock_http_delete.text.return_value = text_output
+        mock_http_delete.status_code = status_code
+        mock_http_delete.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_delete', return_value=mock_http_delete)
 
     def mock_http_put(self, status_code: int, json_output: dict | list = None, text_output: str = '') -> unittest_MagicMock:
-        mock_autoprimary_put = self.mocker.MagicMock(spec=requests.Response)
-        mock_autoprimary_put.json.return_value = json_output
-        mock_autoprimary_put.text.return_value = text_output
-        mock_autoprimary_put.status_code = status_code
-        mock_autoprimary_put.headers = {'Content-Type': 'application/json'}
-        return self.mocker.patch('powerdns_cli.utils.http_put', return_value=mock_autoprimary_put)
+        mock_http_put = self.mocker.MagicMock(spec=requests.Response)
+        mock_http_put.json.return_value = json_output
+        mock_http_put.text.return_value = text_output
+        mock_http_put.status_code = status_code
+        mock_http_put.headers = {'Content-Type': 'application/json'}
+        return self.mocker.patch('powerdns_cli.utils.http_put', return_value=mock_http_put)
